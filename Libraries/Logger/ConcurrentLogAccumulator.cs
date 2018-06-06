cc37c742a827f831987b2c1dcde883a173eacc96
Ilfat <ilfat.sabirzyanov@raxeltelematics.com>
2018-06-06 11:22:39 +0300

    added logger files

---
+++ Libraries/Logger/ConcurrentLogAccumulator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries.Log
+{
+    public sealed class ConcurrentLogAccumulator : ILogger
+    {
+        private static ConcurrentLogAccumulator instance = null;
+        private static readonly object _syncObjectStatic = new object();
+
+
+        private List<string> _logs = new List<string>();
+        private object _syncObject = new object();
+
+        private ConcurrentLogAccumulator()
+        {
+        }
+
+        public static ConcurrentLogAccumulator GetLogger
+        {
+            get
+            {
+                lock (_syncObjectStatic)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ConcurrentLogAccumulator();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            lock (_syncObject)
+            {
+                _logs.Add($"{DateTime.Now} message : {message}");
+            }
+        }
+
+        public List<String> Release()
+        {
+            lock (_syncObject)
+            {
+                var logs = _logs;
+                _logs = new List<string>();
+                return logs;
+            }
+        }
+
+        public void Log(string message, Exception exception)
+        {
+            lock (_syncObject)
+            {
+                _logs.Add($"{DateTime.Now} message : {message} exception : {exception}");
+            }
+        }
+    }
+}
---
+++ Libraries/Logger/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Libraries.Log
+{
+    public interface ILogger
+    {
+        void Log(string message);
+        void Log(string message, Exception exception);
+        List<string> Release();
+    }
+}
